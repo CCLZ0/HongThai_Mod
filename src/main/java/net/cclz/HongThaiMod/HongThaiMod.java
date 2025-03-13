970fed2252de14e17d61468dfdcdfb6bfb98fcfe
CCLZ0 <czchia06@gmail.com>
2025-03-14 02:46:29 +0800

    Added item is shown properly

--- src/main/java/net/cclz/HongThaiMod/HongThaiMod.java
+++ src/main/java/net/cclz/HongThaiMod/HongThaiMod.java
@@ -1,7 +1,9 @@
 package net.cclz.HongThaiMod;
 
 import com.mojang.logging.LogUtils;
+import net.cclz.HongThaiMod.item.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -15,15 +17,17 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(HongThaiMod.MODID)
+@Mod(HongThaiMod.MOD_ID)
 public class HongThaiMod {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "hongthaimod";
+    public static final String MOD_ID = "hongthaimod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public HongThaiMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -42,24 +46,23 @@ public class HongThaiMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS){
+            event.accept(ModItems.HONGTHAI);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
+
+    }
     }
 }
--- src/main/java/net/cclz/HongThaiMod/item/ModItems.java
+++ src/main/java/net/cclz/HongThaiMod/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.cclz.HongThaiMod.item;
 
+import net.cclz.HongThaiMod.HongThaiMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HongThaiMod.MOD_ID);
+
+    public static final RegistryObject<Item> HONGTHAI = ITEMS.register("hongthai",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
